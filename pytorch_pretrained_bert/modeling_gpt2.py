de058c47ec4762430ef7cd86452de9b9a7545d38
n.pronkin <n.pronkin@tinkoff.ru>
2019-06-08 22:32:10 +0300

    Cls head added

--- pytorch_pretrained_bert/modeling_gpt2.py
+++ pytorch_pretrained_bert/modeling_gpt2.py
@@ -630,6 +630,29 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
             return loss
         return lm_logits, presents
 
+    
+class GPT2ClassificationHeadModel(GPT2PreTrainedModel):
+
+    def __init__(self, config, clf_dropout=0.4, n_class=8):
+        super(GPT2ClassificationHeadModel, self).__init__(config)
+        self.transformer = GPT2Model(config)
+        self.dropout = nn.Dropout(clf_dropout)
+        self.linear = nn.Linear(config.n_embd * 2, n_class)
+
+        nn.init.normal_(self.linear.weight, std = 0.02)
+        nn.init.normal_(self.linear.bias, 0)
+        
+        self.apply(self.init_weights)
+
+
+    def forward(self, input_ids, position_ids=None, token_type_ids=None, lm_labels=None, past=None):
+        hidden_states, presents = self.transformer(input_ids, position_ids, token_type_ids, past)
+        avg_pool = torch.mean(hidden_states, 1)
+        max_pool, _ = torch.max(hidden_states, 1)
+        h_conc = torch.cat((avg_pool, max_pool), 1)
+        logits = self.linear(self.dropout(h_conc))
+        return logits
+
 
 class GPT2DoubleHeadsModel(GPT2PreTrainedModel):
     """OpenAI GPT-2 model with a Language Modeling and a Multiple Choice head ("Language Models are Unsupervised Multitask Learners").
