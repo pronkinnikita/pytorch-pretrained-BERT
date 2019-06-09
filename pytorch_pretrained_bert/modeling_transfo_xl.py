0d60bab4ad9939cf6abbbd9aa03a74ee919062de
n.pronkin <n.pronkin@tinkoff.ru>
2019-06-09 11:52:46 +0300

    clf head for transfo xl added

--- pytorch_pretrained_bert/modeling_transfo_xl.py
+++ pytorch_pretrained_bert/modeling_transfo_xl.py
@@ -1385,3 +1385,26 @@ class TransfoXLLMHeadModel(TransfoXLPreTrainedModel):
 
         # We transpose back
         return (softmax_output, new_mems)
+
+    
+class TransfoXLClassificationHeadModel(TransfoXLPreTrainedModel):
+    def __init__(self, config, clf_dropout=0.4, n_class=8):
+        super(TransfoXLClassificationHeadModel, self).__init__(config)
+        self.transformer = TransfoXLModel(config)
+        self.dropout = nn.Dropout(clf_dropout)
+        self.linear = nn.Linear(config.d_embed * 2, n_class)
+
+        nn.init.normal_(self.linear.weight, std = 0.02)
+        nn.init.normal_(self.linear.bias, 0)
+        
+        self.apply(self.init_weights)
+
+    def forward(self, input_ids, target=None, mems=None):
+
+        last_hidden, _ = self.transformer(input_ids, mems)
+        avg_pool = torch.mean(last_hidden, 1)
+        max_pool, _ = torch.max(last_hidden, 1)
+        h_conc = torch.cat((avg_pool, max_pool), 1)
+        logits = self.linear(self.dropout(h_conc))
+
+        return logits
